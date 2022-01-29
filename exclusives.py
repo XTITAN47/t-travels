e75f3a9233dc5845b8ef07b6653f2a27270c3210
Dang3Rsm <87050743+Dang3Rsm@users.noreply.github.com>
2022-01-29 22:49:45 +0530

    Update exclusives.py

--- exclusives.py
+++ exclusives.py
@@ -33,7 +33,7 @@ def DatabaseDoesNotExist():
     cu.execute('CREATE DATABASE titan_travels;')
     cu.execute('use titan_travels;')
     cu.execute('CREATE TABLE users (user varchar(10),contact varchar(11),email varchar(255),psw varchar(255),adminstat varchar(255),primary key(user));')
-    cu.execute('CREATE TABLE aeroplanes (flightid varchar(10),flightname varchar(255),departure varchar(255),destination varchar(255),primary key(flightno));')
+    cu.execute('CREATE TABLE aeroplanes (flightid varchar(10),flightname varchar(255),departure varchar(255),destination varchar(255),primary key(flightid));')
     cu.execute('CREATE TABLE hotels (hotelid varchar(10),hotelname varchar(255),address varchar(11),rooms varchar(255),primary key(hotelid));')
     cu.execute('CREATE TABLE buses (busid varchar(10),departure varchar(255),destination varchar(255),primary key(busid));')
     cu.execute('CREATE TABLE trains (trainid varchar(10),trainname varchar(255),departure varchar(11),destination varchar(255),primary key(trainid));')
