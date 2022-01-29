aa0a74a4e1ca1e0f85d3c04210ab8cddbee99384
Dang3Rsm <87050743+Dang3Rsm@users.noreply.github.com>
2022-01-30 04:12:16 +0530

    Update air_tickets.py

--- air_tickets.py
+++ air_tickets.py
@@ -3,48 +3,32 @@ from tkinter import ttk
 import csv
 from PIL import Image, ImageTk
 from tkinter.font import Font
+import mysql.connector
+
 def air():
+    cu = mydb.cursor()
+    cu.execute('SELECT * FROM AEROPLANES;')
+    data = cu.fetchall()
+    noOfAvFlights = 0
+    departure=[]
+    destination=[]
+    for i in data:
+        noOfAvFlights +=1
+        if i[2] not in departure:
+            departure.append(i[2])
+        if i[3] not in destination:
+            destination.append(i[3])
+        print(i)
+
 #================================================== SEARCH and DISPLAY ===================================================
-
     def search():
-        dep=combo.get()
-        arr=combo1.get()
-        frame23=Frame(airwin,width=590,height=400,highlightbackground="white",highlightthickness=2)
-        frame23.place(x=400,y=350)
+        frame23=Frame(airwin,width=900,height=300,highlightbackground="black",highlightthickness=2)
+        frame23.place(x=200,y=300)
+        l1=Label(frame22,text="FROM",font=bigfont2)
 
-        tt=open(r"data\csvfiles\airt.csv","r",newline="\r\n")
-        all_flights=csv.reader(tt)
-        AVAILABLE=[]
         
-        for i in all_flights:
-            a=str(i[0]) ; b=str(i[1])
 
-            if a!=str(dep) and b!=str(arr):
-                pass
-            else:
-                AVAILABLE.append(i)
-        
-        def display():
-            def ticket_box():
-                flight=var.get()
-                classs=clas.get()
-                
-                T=Text(frame23,height="10",width="80")
-                data="<<<----YOU HAVE SUCCESSFULLY BOOKED YOUR AIR TICKET ---->>"+"\n"+AVAILABLE[flight][0]+"   TO   "+"\t"+AVAILABLE[flight][1]+"\nDEPARTURE : "+AVAILABLE[flight][2]+"\tARRIVAL : "+AVAILABLE[flight][3]+"\nYOU HAVE TO PAY Rs.  "+AVAILABLE[flight][classs]
-                T.insert(END,data)
-                T.grid(row="15",column="1")
-                            
-            clas=IntVar()
-            r1=Radiobutton(frame23,text="ECONOMY CLASS",variable=clas,value=4,command=ticket_box).grid(row=str(x),column="1")
-            r1=Radiobutton(frame23,text="BUSSINESS CLASS",variable=clas,value=5,command=ticket_box).grid(row=str(x+1),column="1")
-            r1=Radiobutton(frame23,text="FIRST CLASS",variable=clas,value=6,command=ticket_box).grid(row=str(x+2),column="1")
-                      
-        var=IntVar()
-        x=7
-        for i in AVAILABLE:
-            a=i[0]+'  TO  ,'+i[1]+"  DEPRATURE  : "+i[2]+"  ARRIVAL  : "+i[3]+'\n\nECONOMY CLASS    ----&----'+i[4]+'\t  BUSSINESS CLASS    ----&---- '+i[5]+'\tFIRST CLASS   ----&----   '+i[6]
-            r1=Radiobutton(frame23,text=a,variable=var,value=AVAILABLE.index(i),command=display).grid(row=str(x),column="1")
-            x=x+1
+        pass
 
 #===============================================================================================================================                                             
                     
@@ -67,18 +51,27 @@ def air():
     frame21.place(x=250,y=30)
     frame22=Frame(airwin,width=900,height=100,highlightbackground="black",highlightthickness=2)
     frame22.place(x=200,y=180)
-    l3=Label(frame21,text="....INDIAN AIRWAYS.....",font=bigfont,bg="#4b2aa1",fg="white").place(x=150,y=10)
-    l1=Label(frame22,text="FROM",font=bigfont2).place(x=20,y=20)
-    l2=Label(frame22,text="TO",font=bigfont2).place(x=600,y=30)
+    l3=Label(frame21,text="....INDIAN AIRWAYS.....",font=bigfont,bg="#4b2aa1",fg="white")
+    l3.place(x=150,y=10)
+    l1=Label(frame22,text="FROM",font=bigfont2)
+    l1.place(x=20,y=20)
+    l2=Label(frame22,text="TO",font=bigfont2)
+    l2.place(x=600,y=30)
    
-    combo= ttk.Combobox(frame22,value=["Departure","DELHI ","JAIPUR ","MUMBAI ","AGRA","KOLKATA"])
+    combo= ttk.Combobox(frame22,value=departure)
     combo.current(0)
     combo.place(x=200,y=45)
-    combo1= ttk.Combobox(frame22,value=["Destination","DELHI","JAIPUR","MUMBAI","AGRA"])
+    combo1= ttk.Combobox(frame22,value=destination)
     combo1.current(0)
     combo1.place(x=700,y=45)
-    button=Button(airwin,text="SEARCH",command=search).place(x=550,y=300)
+    button=Button(airwin,text="SEARCH",command=search,width=126)
+    button.place(x=202,y=280)
     airwin.mainloop()
 
 if __name__ == '__main__':
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Titan@1067",
+    database='titan_travels')
     air()
