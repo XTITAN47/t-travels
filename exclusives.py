40e5926e504d09855d6295ceb95700599eed0575
Dang3Rsm <87050743+Dang3Rsm@users.noreply.github.com>
2022-02-03 22:44:19 +0530

    Update exclusives.py

--- exclusives.py
+++ exclusives.py
@@ -3,10 +3,12 @@ import mysql.connector
 from database import *
 
 from database import pushDBtoDAT
+
+PASS = 'Titan@1067'
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Titan@1067",
+  password=PASS,  database='titan_travels'
 )
 
 def validateUserReg(a):
@@ -34,11 +36,11 @@ def DatabaseDoesNotExist():
     cu.execute('use titan_travels;')
     cu.execute('CREATE TABLE users (user varchar(10),contact varchar(11),email varchar(255),psw varchar(255),adminstat varchar(255),primary key(user));')
     cu.execute('CREATE TABLE aeroplanes (flightid varchar(10),flightname varchar(255),departure varchar(255),destination varchar(255),primary key(flightid));')
-    cu.execute('CREATE TABLE hotels (hotelid varchar(10),hotelname varchar(255),address varchar(11),rooms varchar(255),primary key(hotelid));')
+    cu.execute('CREATE TABLE hotels (hotelid varchar(10),hotelname varchar(255),address varchar(11),family varchar(255), single varchar(255),doubleBed varchar(255),checkin varchar(255), checkout varchar(255),primary key(hotelid));')
     cu.execute('CREATE TABLE buses (busid varchar(10),departure varchar(255),destination varchar(255),primary key(busid));')
     cu.execute('CREATE TABLE trains (trainid varchar(10),trainname varchar(255),departure varchar(11),destination varchar(255),primary key(trainid));')
     cu.execute('CREATE TABLE airtickets (airticketid varchar(10),user varchar(10),flightno varchar(10),username varchar(255), flightname varchar(255),departure varchar(255),destination varchar(255),price int(11),primary key(airticketid));')
-    cu.execute('CREATE TABLE hoteltickets (hotelticketid varchar(10),user varchar(10),hotelid varchar(10),username varchar(255), hotelname varchar(255),address varchar(255),price int(11),primary key(hotelticketid));')
+    cu.execute('create table hoteltickets(hotelticketid varchar(10), user varchar(10), hotelid varchar(10), fname varchar(255), hotelname varchar(255), city varchar(255), roomtype varchar(255), checkin varchar(255), checkout varchar(255), price varchar(255), primary key(hotelticketid));')
     cu.execute('CREATE TABLE bustickets (busticketid varchar(10),user varchar(10),busid varchar(10),username varchar(255),departure varchar(255),destination varchar(255),price int(11),primary key(busticketid));')
     cu.execute('CREATE TABLE traintickets (trainticketid varchar(10),user varchar(10),trainid varchar(10),username varchar(255),trainname varchar(255),departure varchar(255),destination varchar(255),price int(11),primary key(trainticketid));')
     cu.execute('CREATE TABLE reviews (reviewid varchar(10),user varchar(10),username varchar(255),review varchar(255),primary key(reviewid));')
